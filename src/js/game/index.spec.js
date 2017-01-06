7aee64cb9db11772d1f8c19aef704462cfd09054
Xesenix <pkapalla@xesenix.pl>
2017-01-06 01:27:06 +0100

    it is better to use function instead of lambdas for test so you can have access to valid this inside test cases and we don`t need to initialize game before each test case it enough for initialize it once before all tests

--- src/js/game/index.spec.js
+++ src/js/game/index.spec.js
@@ -6,47 +6,47 @@ import PlayState from 'js/game/states/play'
 
 import gameConstructor from 'js/game/index'
 
-describe('Game Constructor', () => {
+describe('Game Constructor', function() {
 	const element = document.createElement('div')
 	let game
 	
-	beforeEach(() => {
+	before(() => {
 		// for testing only Phaser.WEBGL works and yes i know about Phaser.HEADLESS
 		game = gameConstructor(640, 480, Phaser.WEBGL, element)
 		// dont wait for event
 		game.boot()
 	})
 	
-	afterEach(() => {
+	after(() => {
 		game.destroy()
 	})
 	
-	it('should contain preload state', () => {
+	it('should contain preload state', function() {
 		expect(game.state.states.preload).to.be.instanceOf(PreloadState)
 	})
 	
-	it('should contain boot state', () => {
+	it('should contain boot state', function() {
 		expect(game.state.states.boot).to.be.instanceOf(BootState)
 	})
 	
-	it('should contain intro state', () => {
+	it('should contain intro state', function() {
 		expect(game.state.states.intro).to.be.instanceOf(IntroState)
 	})
 	
-	it('should contain menu state', () => {
+	it('should contain menu state', function() {
 		expect(game.state.states.menu).to.be.instanceOf(MenuState)
 	})
 	
-	it('should contain play state', () => {
+	it('should contain play state', function() {
 		expect(game.state.states.play).to.be.instanceOf(PlayState)
 	})
 	
-	it('should be of constructed size', () => {
+	it('should be of constructed size', function() {
 		expect(game.width).to.be.equal(640)
 		expect(game.height).to.be.equal(480)
 	})
 	
-	it('should be initialized in choosen dom element', () => {
+	it('should be initialized in choosen dom element', function() {
 		expect(game.parent).to.be.equal(element)
 	})
 })
